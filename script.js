562888a6e3716f45a8a45840c49ff5ca87360a3c
Brandon <98434819+Brandon2023@users.noreply.github.com>
2022-04-08 23:23:38 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function togglePopup(){
+    document.getElementById("popup").classList.toggle("active");
+}
+function togglePopup2(){
+    document.getElementById("popup2").classList.toggle("active");
+}
+function togglePopup3(){
+    document.getElementById("popup3").classList.toggle("active");
+}
